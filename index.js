52df233228fb38101c312b566b33920fe0857b8a
James Manansala <edward.dyamez@outlook.com>
2023-03-09 20:40:32 -0800

    3_update030923

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 
 let content = document.querySelector('.js-generated.content')
-
+/*
 let header = document.createElement('h1')
 header.setAttribute('class', 'dog-name')
 header.append('Rizzo')
 content.append(header)
+*/
+
+
+
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')
+dogcontent.append(header)
 
 let dogContent = document.createElement('div')
 dogContent.setAttribute('class', 'dog-content')
-content.append(dogContent)
+dogContent.append(dogContent)
 
 let dogImage = document.createElement('img')
 dogImage.setAttribute('class', 'dog-image')
 dogImage.setAttribute('src', './assets/rizzo.jpg')
-content.append(dogImage)
+dogContent.append(dogImage)
 
 let dogDetails = document.createElement('div')
 dogDetails.setAttribute('class', 'dog-details')
-content.append(dogDetails)
+dogContent.append(dogDetails)
 
+let descHeadline = document.createElement('h3')
+descHeadline.innerText = 'description';
+dogDetails.append(descHeadline);
 
+let descParagraph =  document.createElement('p');
+descParagraph.innerText = "This gentle dog is aloof"
++ "toward her owner", 
++ "and never comes when called."
++ "She always acts as though any stranger"
++ "she meets will harm her, and"
++ "dislikes other animals."
+dogDetails.append(descParagraph);
